9118591da5066f1f8befa964a37e5522ade1b6a0
Yacinn12 <josephdiasso6@gmail.com>
2024-07-09 11:09:02 +0000

    première modification

--- les_fonctions/fonctions.py
+++ les_fonctions/fonctions.py
@@ -1,69 +1,64 @@
-# Créez une liste vide pour le panier
-panier = []
-
-# Fonction pour afficher le menu
 def afficher_menu():
-    print("""Choisissez parmi les 5 options suivantes:
-                1- Ajouter un article dans le panier
-                2- Supprimer un article du panier
-                3- Afficher tous les articles
-                4- Vider le panier
-                5- Quitter""")
-    
+    print("\nChoisissez parmi les 5 options suivantes:")
+    print("1- Ajouter un article dans le panier")
+    print("2- Supprimer un article du panier")
+    print("3- Afficher tous les articles")
+    print("4- Vider le panier")
+    print("5- Quitter")
 
+def ajouter_article(panier):
+    nom_article = input("Entrez le nom de l'article: ")
+    prix_article = float(input("Entrez le prix de l'article: "))
+    article = {'name': nom_article, 'price': prix_article}
+    panier.append(article)
+    print(f"Article {nom_article} ajouté avec succès!")
 
-# Fonction pour ajouter un article au panier
-def ajouter_article():
-    article = input("Entrez le nom de l'article : ")
-    prix = float(input("Entrez le prix de l'article : "))
-    nouvel_article = {"name": article, "price": prix}
-    panier.append(nouvel_article)
-    print(f"{article} a été ajouté au panier.")
-
-# Fonction pour supprimer un article du panier
-def supprimer_article():
-    article_supprime = input("Entrez le nom de l'article à supprimer : ")
+def supprimer_article(panier):
+    nom_article = input("Entrez le nom de l'article à supprimer: ")
     article_trouve = False
     for article in panier:
-        if article["name"] == article_supprime:
+        if article['name'] == nom_article:
             panier.remove(article)
             article_trouve = True
-            print(f"{article_supprime} a été supprimé du panier.")
-            
+            print(f"Article {nom_article} supprimé avec succès!")
+            break
     if not article_trouve:
-        print("Aucun article avec ce nom dans le panier.")
+        print("Aucun article avec ce nom. ")
 
-# Fonction pour afficher tous les articles du panier
-def afficher_articles():
-    for article in panier:
-        print(f"{article['name']} - Prix : {article['price']}")
+def afficher_articles(panier):
+    if panier:
+        print("Voici les articles dans votre panier:")
+        for article in panier:
+            print(f"Article: {article['name']}, Prix: {article['price']}")
+    else:
+        print("Votre panier est vide.")
 
-# Fonction pour vider le panier
-def vider_panier():
+def vider_panier(panier):
     panier.clear()
-    print("Le panier a été vidé.")
+    print("Le panier a été vidé avec succès!")
 
-# Boucle principale du programme
+# Créer une variable panier qui est une liste vide
+panier = []
+
 while True:
     afficher_menu()
-    choix = input("Quel est votre choix : ")
+    choix = input("Quel est votre choix? ")
 
-while not choix.isdigit():
-    print("Choisissez uniquement un élément existant !!!")
-    print("Quel est votre choix : ")
+    if choix == '1':
+        ajouter_article(panier)
 
-choix = int(choix) 
+    elif choix == '2':
+        supprimer_article(panier)
 
-if choix == 1:
-        ajouter_article()
-elif choix == 2:
-            supprimer_article()
-elif choix == 3:
-            afficher_articles()
-elif choix == 4:
-            vider_panier()
-elif choix == 5:
-            print("Fin du programme.")
-            
-else:
-            print("Choix invalide. Veuillez sélectionner une option valide.")
+    elif choix == '3':
+        afficher_articles(panier)
+
+    elif choix == '4':
+        vider_panier(panier)
+
+    elif choix == '5':
+        print("Fin du programme")
+        exit()
+
+    else:
+        print("Choix invalide, veuillez réessayer.")
